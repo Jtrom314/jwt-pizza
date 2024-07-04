bb289119fe8ef03336b1d8b23307abe5aaf42ef1
Jtrom314 <97546727+Jtrom314@users.noreply.github.com>
2024-07-04 12:54:04 -0600

    D4 Added more tests: 64% coverage

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -125,3 +125,82 @@ test('register a new user', async ({ page }) => {
 
   await page.getByRole('button', { name: 'Register' }).click()
 })
+
+// test('admin dashboard works correctly', async ({ page }) => {
+
+//   await page.route('*/**/api/auth', async (route) => {
+//     const loginReq = { email: 'd@jwt.com', password: 'a' };
+//     const loginRes = { id: 3, name: 'Kai Chen', email: 'd@jwt.com', roles: [{ role: 'admin' }] };
+//     expect(route.request().method()).toBe('PUT');
+//     expect(route.request().postDataJSON()).toMatchObject(loginReq);
+//     await route.fulfill({ json: loginRes });
+//   });
+
+//   await page.goto('http://localhost:5173')
+// })
+
+test('docs work correctly', async ({ page }) => {
+  await page.route('*/**/api/docs', async (route) => {
+    expect(route.request().method()).toBe('GET')
+  })
+
+  await page.goto('http://localhost:5173/docs')
+})
+
+test('about page loads correctly', async ({ page }) => {
+  await page.goto('http://localhost:5173/about');
+  await page.getByText('The secret sauce')
+  await page.getByRole('main').getByRole('img').first()
+  await page.locator('div').filter({ hasText: /^James$/ }).getByRole('img')
+  await page.locator('div').filter({ hasText: /^Maria$/ }).getByRole('img')
+  await page.locator('div').filter({ hasText: /^Anna$/ }).getByRole('img')
+  await page.locator('div').filter({ hasText: /^Brian$/ }).getByRole('img')
+})
+
+test('history page loads correctly', async ({ page }) => {
+  await page.goto('http://localhost:5173/history')
+  expect(page.getByText('Mama Rucci, my my')).toBeVisible()
+})
+
+test('logout page functions properly', async ({ page }) => {
+  // We have to first login so that we can logout
+  await page.route('*/**/api/auth', async (route) => {
+    const userReq = {
+      name: 'user',
+      password: 'a',
+      email: 'a@jwt.org'
+    }
+    const registerRes = {}
+    expect(route.request().method()).toBe('POST');
+    expect(route.request().postDataJSON()).toMatchObject(userReq)
+    await route.fulfill({ json: registerRes });
+  });
+
+  await page.goto('http://localhost:5173/')
+
+  // Go to register page
+  await page.getByRole('link', { name: 'Register' }).click()
+
+  // fill in register infomration
+
+  await page.getByPlaceholder('Full name').click()
+  await page.getByPlaceholder('Full name').fill('user')
+  await page.getByPlaceholder('Full name').press('Tab')
+
+  await page.getByPlaceholder('Email address').click()
+  await page.getByPlaceholder('Email address').fill('a@jwt.org')
+  await page.getByPlaceholder('Email address').press('Tab')
+
+  await page.getByPlaceholder('Password').click()
+  await page.getByPlaceholder('Password').fill('a')
+  await page.getByPlaceholder('Password').press('Tab')
+  await page.getByRole('button', { name: 'Register' }).click()
+  
+  await page.goto('http://localhost:5173')
+  
+  await page.route('*/**/api/auth', async (route) => {
+    expect(route.request().method()).toBe('DELETE')
+  })
+
+  await page.goto('http://localhost:5173/logout')
+})
